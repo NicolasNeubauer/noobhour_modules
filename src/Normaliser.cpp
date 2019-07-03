1cf780c03bcdb8df28358b5a5dd7a045ada8ab95
Nicolas Neubauer <nicolas.neubauer@gmail.com>
2019-07-03 02:03:57 +0200

    UI works first time

--- src/Normaliser.cpp
+++ src/Normaliser.cpp
@@ -1,5 +1,5 @@
-#include "Noobhour.hpp"
 #include "plugin.hpp"
+#include "Noobhour.hpp"
 
 
 struct Normaliser : Module {
@@ -43,21 +43,19 @@ struct NormaliserWidget : ModuleWidget {
 		setModule(module);
 		setPanel(APP->window->loadSvg(asset::plugin(pluginInstance, "res/Normaliser.svg")));
 
-		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(19.2, 92.294)), module, Normaliser::MIN_PARAM));
-		/*
-		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(19.2, 137.578)), module, Normaliser::MAX_PARAM));
-		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(19.2, 182.76)), module, Normaliser::ABS_PARAM));
+		addParam(createParamCentered<RoundSmallBlackKnob>(mm2px(Vec(10.16/2, 128.5-(100.112+7.937/2))), module, Normaliser::MIN_PARAM));
+		addParam(createParamCentered<RoundSmallBlackKnob>(mm2px(Vec(10.16/2, 128.5-(88.13 + 7.937/ 2))), module, Normaliser::MAX_PARAM));
+		addParam(createParamCentered<RoundSmallBlackKnob>(mm2px(Vec(10.16/2, 128.5-(76.176 + 7.937/ 2))), module, Normaliser::ABS_PARAM));
 
-		// addInput(createInputCentered<PJ301MPort>(mm2px(Vec(19.2, -66.891)), module, Normaliser::IN_INPUT));
+		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(10.16/2, 128.5-(112.000 + 7.858 / 2))), module, Normaliser::IN_INPUT));
 
-		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(19.2, 360.39)), module, Normaliser::CURRENTMIN_OUTPUT));
-		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(19.2, 400.286)), module, Normaliser::CURRENTMAX_OUTPUT));
-		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(19.2, 440.414)), module, Normaliser::OUT_OUTPUT));
+		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(10.16/2, 128.5-(28.424 + 8.026 / 2))), module, Normaliser::CURRENTMIN_OUTPUT));
+		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(10.16/2, 128.5-(17.868 + 8.026 / 2))), module, Normaliser::CURRENTMAX_OUTPUT));
+		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(10.16/2, 128.5-(7.317 + 7.761 / 2))), module, Normaliser::OUT_OUTPUT));
 
-		addChild(createLightCentered<MediumLight<RedLight>>(mm2px(Vec(19.177, 236.876)), module, Normaliser::RESET_LIGHT));
-		addChild(createLightCentered<MediumLight<RedLight>>(mm2px(Vec(19.2, 269.295)), module, Normaliser::FREEZE_LIGHT));
-		addChild(createLightCentered<MediumLight<RedLight>>(mm2px(Vec(19.2, 304.296)), module, Normaliser::POLY_LIGHT));
-		*/
+		addChild(createLightCentered<MediumLight<RedLight>>(mm2px(Vec(10.16/2, 128.5-(63.822 + 4.009/ 2))), module, Normaliser::RESET_LIGHT));
+		addChild(createLightCentered<MediumLight<RedLight>>(mm2px(Vec(10.16/2, 128.5-(55.245 + 4.009 / 2))), module, Normaliser::FREEZE_LIGHT));
+		addChild(createLightCentered<MediumLight<RedLight>>(mm2px(Vec(10.16/2, 128.5-(45.984 + 4.009 / 2))), module, Normaliser::POLY_LIGHT));
 	}
 };
 
